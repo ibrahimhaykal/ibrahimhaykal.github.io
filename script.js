b8110ecbb65868ed9aa8cea7efeb9941d5d31809
Ibrahim Haykal <120263627+ibrahimhaykal@users.noreply.github.com>
2024-03-17 13:20:21 +0700

    change

--- script.js
+++ script.js
@@ -7,28 +7,3 @@
         refresh = setTimeout("action()", speed);
       }
       action();
-
-      const texts = [
-        { element: document.getElementById('hello'), text: 'Hello,', delay: 100 },
-        { element: document.getElementById('name'), text: 'I am Ibrahim Haykal', delay: 100 },
-        { element: document.getElementById('lastname'), text: 'Alatas', delay: 100 },
-        { element: document.getElementById('description1'), text: 'Web Developer || IT Enthusiast', delay: 100 },
-        { element: document.getElementById('description2'), text: 'Student || Cyber Security Learner', delay: 100 }
-      ];
-      
-      function typewriter(text, index) {
-        let i = 0;
-        let interval = setInterval(() => {
-          text.element.innerHTML += text.text.charAt(i);
-          i++;
-          if (i > text.text.length) {
-            clearInterval(interval);
-            if (index + 1 < texts.length) {
-              typewriter(texts[index + 1], index + 1);
-            }
-          }
-        }, text.delay);
-      }
-      
-      typewriter(texts[0], 0);
-      
