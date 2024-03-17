819e50bdb944e5d82345c9a516af35aca54cfcde
Ibrahim Haykal <120263627+ibrahimhaykal@users.noreply.github.com>
2024-03-17 14:50:25 +0700

    Edit Typewriter

--- script.js
+++ script.js
@@ -7,14 +7,14 @@
         refresh = setTimeout("action()", speed);
       }
       action();
-      var i = 0, text;
-      text = "Hello";
-      function typewriter()
-      {
-        if (i < text.length){
-            document.getElementById ('hello').innerHTML += text.charAt(i);
-            i++;
-            setTimeout (typewriter, 50)
-        }
-      }
-      typewriter()
+
+var i = 0, text;
+text = "Hello";
+function typewriter() {
+    if (i < text.length) {
+        document.getElementById('hello').innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typewriter, 50);
+    }
+}
+typewriter();
