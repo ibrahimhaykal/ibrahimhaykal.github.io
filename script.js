868bd723f723baf2f1bda18964576af46e97710c
Ibrahim Haykal <120263627+ibrahimhaykal@users.noreply.github.com>
2024-03-17 15:00:40 +0700

    Nav Active Change

--- script.js
+++ script.js
@@ -7,3 +7,16 @@
         refresh = setTimeout("action()", speed);
       }
       action();
+
+      document.addEventListener("DOMContentLoaded", function() {
+        const navLinks = document.querySelectorAll(".nav-link");
+    
+        navLinks.forEach(function(navLink) {
+          navLink.addEventListener("click", function() {
+            navLinks.forEach(function(link) {
+              link.classList.remove("active");
+            });
+            this.classList.add("active");
+          });
+        });
+      });
